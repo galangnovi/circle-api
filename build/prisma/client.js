5378022e9f46a4f4f7e98686684bab4dba613083
galangnovi <galangnovi21@gmail.com>
2025-08-02 20:23:40 +0700

    updated 10

--- build/prisma/client.js
+++ build/prisma/client.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-const prisma_1 = require("../generated/prisma");
+const prisma_1 = require("../../src/generated/prisma");
 exports.prisma = new prisma_1.PrismaClient();
