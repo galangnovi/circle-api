4aa518435292594e0fc006c2b6ffa6119e719fbe
galangnovi <galangnovi21@gmail.com>
2025-08-02 12:10:20 +0700

    updated 2

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(); // default localhost:6379, atau bisa tambahkan config
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379'); // default localhost:6379, atau bisa tambahkan config
 
 export default redis;
